7d326752d38ebc096a6c0846371099d4f2e358f7
Jonathan Verbeek <iredcraft@gmail.com>
2020-04-30 20:27:42 +0200

    Cleaned up the libnx template

--- source/main.c
+++ source/main.c
@@ -1,3 +1,8 @@
+/*
+    NXLightSwitch for Nintendo Switch
+    Made with love by Jonathan Verbeek (jverbeek.de)
+*/
+
 // Include the most common headers from the C standard library
 #include <stdio.h>
 #include <stdlib.h>
@@ -5,24 +10,23 @@
 
 // Include the main libnx system header, for Switch development
 #include <switch.h>
-#include "wait.h"
 
-// Sysmodules should not use applet*.
+// Sysmodules should not use applet*
 u32 __nx_applet_type = AppletType_None;
 
-// Adjust size as needed.
+// Adjust heap size as needed
 #define INNER_HEAP_SIZE 0x80000
 size_t nx_inner_heap_size = INNER_HEAP_SIZE;
 char   nx_inner_heap[INNER_HEAP_SIZE];
 
-UTimer* timer;
-
+// Called by libnx to initialize the heap memory we get for this sysmodule
 void __libnx_initheap(void)
 {
+    // Original NX Heap memory
 	void*  addr = nx_inner_heap;
 	size_t size = nx_inner_heap_size;
 
-	// Newlib
+	// Fake Heap which we could use for our own data
 	extern char* fake_heap_start;
 	extern char* fake_heap_end;
 
@@ -30,44 +34,53 @@ void __libnx_initheap(void)
 	fake_heap_end   = (char*)addr + size;
 }
 
-// Init/exit services, update as needed.
+// Initializes the sysmodule, used to init services
 void __attribute__((weak)) __appInit(void)
 {
+    // Will hold the result of several service inits
     Result rc;
 
-    // Initialize default services.
+    // Initialize default Switch services and make sure it was successful
     rc = smInitialize();
     if (R_FAILED(rc))
+    {
         fatalThrow(MAKERESULT(Module_Libnx, LibnxError_InitFail_SM));
+    }
 
-    // Enable this if you want to use HID.
+    // Enable this if you want to use HID
     /*rc = hidInitialize();
     if (R_FAILED(rc))
         fatalThrow(MAKERESULT(Module_Libnx, LibnxError_InitFail_HID));*/
 
-    //Enable this if you want to use time.
+    //Enable this if you want to use time
     /*rc = timeInitialize();
     if (R_FAILED(rc))
         fatalThrow(MAKERESULT(Module_Libnx, LibnxError_InitFail_Time));
 
     __libnx_init_time();*/
 
+    // Initialize the filesystem service and make sure it was successful
     rc = fsInitialize();
     if (R_FAILED(rc))
+    {
         fatalThrow(MAKERESULT(Module_Libnx, LibnxError_InitFail_FS));
+    }
 
+    // Mount the SDMC storage
     fsdevMountSdmc();
 }
 
+// Called (and not needed because this is a sysmodule) when the user tries to exit this app
 void __attribute__((weak)) userAppExit(void);
 
+// Called when the Switch requests this sysmodule to exit
 void __attribute__((weak)) __appExit(void)
 {
-    // Cleanup default services.
+    // Cleanup and exit the services we opened
     fsdevUnmountAll();
     fsExit();
-    //timeExit();//Enable this if you want to use time.
-    //hidExit();// Enable this if you want to use HID.
+    //timeExit();
+    //hidExit();
     smExit();
 }
 
